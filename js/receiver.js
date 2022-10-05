7dc082e2d6e47684c0c34446bc53532b96e250cf
kakaoent-leo <leo.ee@kakaoent.com>
2022-10-05 15:52:24 +0900

    ts 제거

--- js/receiver.js
+++ js/receiver.js
@@ -201,11 +201,11 @@ playerManager.setMessageInterceptor(
             );
             loadRequestData.media.contentUrl = source;
 
-            loadRequestData.media.contentType = "application/x-mpegurl";
-            loadRequestData.media.hlsSegmentFormat =
-              cast.framework.messages.HlsSegmentFormat.TS;
-            loadRequestData.media.hlsVideoSegmentFormat =
-              cast.framework.messages.HlsVideoSegmentFormat.MPEG2_TS;
+            // loadRequestData.media.contentType = "application/x-mpegurl";
+            // loadRequestData.media.hlsSegmentFormat =
+            //   cast.framework.messages.HlsSegmentFormat.TS;
+            // loadRequestData.media.hlsVideoSegmentFormat =
+            //   cast.framework.messages.HlsVideoSegmentFormat.MPEG2_TS;
 
             // let metadata = new cast.framework.messages.GenericMediaMetadata();
             // metadata.images = [
